e710ae184bd1c3be6c0a136beb0c333d2ab86610
numan <noomaan24@gmail.com>
2024-10-03 03:25:38 +0500

    basic security added. password is now saving in hashed form. user has now roles.

---
+++ src/main/java/com/numan/journalapp/service/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.numan.journalapp.service;
+
+import com.numan.journalapp.entity.UserOfJournal;
+import com.numan.journalapp.repo.UserOfJournalRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserOfJournalRepo userOfJournalRepo;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    UserOfJournal userOfJournal = userOfJournalRepo.findByUserName(username);
+
+    if (userOfJournal != null) {
+      return User.builder()
+        .username(userOfJournal.getUserName())
+        .password(userOfJournal.getPassword())
+        .roles(userOfJournal.getRoles().toArray(new String[0]))
+        .build();
+    }
+
+    throw new UsernameNotFoundException("User not found with user name " + username);
+  }
+}
