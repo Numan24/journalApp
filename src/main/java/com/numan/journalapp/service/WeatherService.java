5d319fa47bc6d9101330a0821c209f941e00f386
numan <noomaan24@gmail.com>
2024-11-15 01:03:49 +0500

    value is used to hide api key.
    @value annotation is used to hide api key from public access.

--- src/main/java/com/numan/journalapp/service/WeatherService.java
+++ src/main/java/com/numan/journalapp/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.numan.journalapp.service;
 
 import com.numan.journalapp.dto.WeatherResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,8 @@ public class WeatherService {
   @Autowired
   private RestTemplate restTemplate;
 
-  private static final String apiKey = "22e812b80502db446c71962a2bfde5c0";
+  @Value("${weather.api.key}")
+  private String apiKey;
   private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
 
