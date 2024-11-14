0053049ebcae7248ed07fe842ee4221e73ff0672
numan <noomaan24@gmail.com>
2024-11-15 01:11:21 +0500

    new rest template config file is created.
    for SOC rest template config file is used to segregate the config file from app start file.

--- src/main/java/com/numan/journalapp/JournalApplication.java
+++ src/main/java/com/numan/journalapp/JournalApplication.java
@@ -3,9 +3,7 @@ package com.numan.journalapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JournalApplication {
