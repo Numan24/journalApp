3eaad548bb3db7748256bb1b70e77612e4c83cb0
numan <noomaan24@gmail.com>
2024-12-02 00:16:33 +0500

    redis cache added
    to save api calls and retrieve data early we used redis as cache to retrieve data as early as possible with the help of key-value pair.

--- src/main/java/com/numan/journalapp/service/WeatherService.java
+++ src/main/java/com/numan/journalapp/service/WeatherService.java
@@ -3,6 +3,7 @@ package com.numan.journalapp.service;
 import com.numan.journalapp.cached.AppCache;
 import com.numan.journalapp.dto.WeatherResponseDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,12 @@ public class WeatherService {
 
   private RestTemplate restTemplate;
   private AppCache appCache;
+  private RedisService redisService;
 
-  WeatherService(RestTemplate restTemplate, AppCache appCache) {
+  WeatherService(RestTemplate restTemplate, AppCache appCache, RedisService redisService) {
     this.restTemplate = restTemplate;
     this.appCache = appCache;
+    this.redisService = redisService;
 
   }
 
@@ -25,8 +28,15 @@ public class WeatherService {
 
 
   public WeatherResponseDTO getWeather(String city) {
+    WeatherResponseDTO weatherResponseDTO = redisService.getFromRedis("weather_of" + city, WeatherResponseDTO.class);
+    if (weatherResponseDTO != null) {
+      return weatherResponseDTO;
+    }
     String finalApi = appCache.AppConfigCache.get(AppCache.AppConfigKeys.WEATHER_API.name()).replace("<city>", city).replace("<apiKey>", apiKey);
     ResponseEntity<WeatherResponseDTO> responseEntity = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponseDTO.class);
+    if (responseEntity.hasBody()) {
+      redisService.setInRedis("weather_of"+ city, responseEntity.getBody(), 3000L);
+    }
     return responseEntity.getBody();
 
   }
